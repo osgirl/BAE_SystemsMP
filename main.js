e4c701fc0e465b0677268299cfa376042e6d9792
Emanuel Inacio <emanuelinacio1@hotmail.com>
2019-01-22 16:36:03 -0700

    p

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 
+// Require the app object from electron
 const {app} = require('electron');
+
+//Require the browser window object. 
 const {BrowserWindow} = require('electron');
 
+// Wait for the app to be ready
+app.on('ready', ()=>{
+    let win = new BrowserWindow({width: 800, height: 400});
+    win.on('closed', ()=>{
+        win = null;
+    });
 
-app.on('ready', function(){
-    let win = new BrowserWindow({width: 800, height: 400})
-})
+});
 
